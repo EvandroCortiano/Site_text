e3948b97652e53b2ebbd530dcb117aadabf089a7
Evandro <evandrocortiano@gmail.com>
2020-01-27 08:03:58 -0300

    First update

---
+++ EmConstrucao/index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Css -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/jcarousel.responsive.css">
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Fonts -->
+	<link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+	<link href='http://fonts.googleapis.com/css?family=Wire+One' rel='stylesheet' type='text/css'>
+
+    <title>Griff Coberturas</title>
+</head>
+<body class="theme-invert">
+    
+<!-- Main (Home) section -->
+<section class="section" id="head">
+	<div class="container">
+
+		<div class="row">
+			<div class="col-md-10 col-lg-10 col-md-offset-1 col-lg-offset-1 text-center">	
+
+				<!-- Site Title, your name, HELLO msg, etc. -->
+				<img src="imagens/logo_v2.png" alt="Logo Griff Coberturas">
+				<h2 class="subtitle">Coberturas em vidro e policarbonato fixa e retrátil!</h2>
+
+				<!-- Short introductory (optional) -->
+				<h3 class="tagline">
+                    Entre em contato: <a href="mailto:contato@griffcoberturas.com.br">contato@griffcoberturas.com.br</a> <br>
+                    Telefone: (41) 98831-9895
+                    <br><br>
+                    Estamos em construção de um novo site ...
+				</h3>
+
+                <!-- Galeria Jquery Carousel -->
+                <div class="wrapper">
+                    <div class="jcarousel-wrapper">
+                        <div class="jcarousel">
+                            <ul>
+                                <li><img src="imagens/img_index/griff_01.jpg" alt="Image 1" width="200px" height="120px"></li>
+                                <li><img src="imagens/img_index/griff_02.jpg" alt="Image 2" width="200px" height="120px"></li>
+                                <li><img src="imagens/img_index/griff_03.jpg" alt="Image 3" width="200px" height="120px"></li>
+                                <li><img src="imagens/img_index/griff_04.jpg" alt="Image 4" width="200px" height="120px"></li>
+                                <li><img src="imagens/img_index/griff_05.jpg" alt="Image 5" width="200px" height="120px"></li>
+                                <li><img src="imagens/img_index/griff_06.jpg" alt="Image 6" width="200px" height="120px"></li>
+                                <li><img src="imagens/img_index/griff_07.jpg" alt="Image 1" width="200px" height="120px"></li>
+                                <li><img src="imagens/img_index/griff_08.jpg" alt="Image 2" width="200px" height="120px"></li>
+                                <li><img src="imagens/img_index/griff_09.jpg" alt="Image 3" width="200px" height="120px"></li>
+                                <li><img src="imagens/img_index/griff_10.jpg" alt="Image 4" width="200px" height="120px"></li>
+                                <li><img src="imagens/img_index/griff_11.jpg" alt="Image 5" width="200px" height="120px"></li>
+                                <li><img src="imagens/img_index/griff_12.jpg" alt="Image 6" width="200px" height="120px"></li>
+                                <li><img src="imagens/img_index/griff_13.jpg" alt="Image 6" width="200px" height="120px"></li>
+                                <li><img src="imagens/img_index/griff_14.jpg" alt="Image 5" width="200px" height="120px"></li>
+                                <li><img src="imagens/img_index/griff_15.jpg" alt="Image 6" width="200px" height="120px"></li>
+                                <li><img src="imagens/img_index/griff_16.jpg" alt="Image 6" width="200px" height="120px"></li>
+                            </ul>
+                        </div>
+                        
+                        <a href="#" class="jcarousel-control-prev">&lsaquo;</a>
+                        <a href="#" class="jcarousel-control-next">&rsaquo;</a>
+                        
+                    </div>
+                </div>
+
+				
+			</div>
+		</div>
+	
+	</div>
+</section>
+
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jcarousel.responsive.js"></script>
+<script src="js/jquery.jcarousel.min.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>
